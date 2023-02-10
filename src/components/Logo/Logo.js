47badbda0a214a74997014059ba95f63dab5929d
Jahmal Senju Camp <senjusama2012@gmail.com>
2023-02-10 12:38:36 -0500

    Smart Brain App created; CSS and Components started

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Tilt from "react-parallax-tilt";
+import './Logo.css';
+import brain from './brain.png'
+
+
+const Logo = () => {
+    return (
+        <div className="ma4 mt0">
+      <Tilt className="Tilt br2 shadow-2" options={{max : 75}} style={{height: '300px', width: '300px',}}>
+        
+        {/* //FIX IMAGE NOT BEING CENTERED! */}
+        <div className="Tilt-inner pa3" style={{paddingTop: '5px'}}>
+          <img src={brain} alt='brain'/>
+        </div>
+      </Tilt>
+    </div>
+  );
+};
+
+export default Logo;
